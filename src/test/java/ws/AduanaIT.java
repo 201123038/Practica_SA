cb25f1229815d37c6a6252ae2d0ab996391fd038
Yessenia <ymjm93@gmail.com>
2017-10-09 20:18:50 -0600

    up to date

--- src/test/java/ws/AduanaIT.java
+++ src/test/java/ws/AduanaIT.java
@@ -35,7 +35,7 @@ public class AduanaIT {
         System.out.println("calcular_Costo_Aduana");
         Aduana instance = new Aduana();
         String result = instance.calcular_Costo_Aduana("linea", 123);
-        assertEquals("hola", result);
+        assertEquals("", result);
         }
 
     /**
